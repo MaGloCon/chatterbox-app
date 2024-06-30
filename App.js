a39008804033a51133cb10e4a5a575762ef522a5
MaGloCon <mgcontrada@gmail.com>
2024-06-30 18:05:27 +0200

    Adjusted indentation for uniformity, adjusted comments

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StyleSheet, View, ImageBackground, TextInput, Text, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Start from './components/Start';
-import Chat from './components/Chat';
+import Start from "./components/Start";
+import Chat from "./components/Chat";
 
 // Create a Stack Navigator
 const Stack = createNativeStackNavigator();
@@ -12,20 +11,12 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Chatterbox"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+      <Stack.Navigator initialRouteName="Chatterbox">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat" component={Chat} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
--- components/Chat.js
+++ components/Chat.js
@@ -1,83 +1,87 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
-// Define the Chat component that takes route and navigation props for navigation and parameter passing
 const Chat = ({ route, navigation }) => {
-    // Extracting name and background color from navigation parameters
-    const { name, background } = route.params;
-    const [messages, setMessages] = useState([]);
+  // Extracting name and background color from navigation parameters
+  const { name, background } = route.params;
 
-    const onSend = (newMessages) => {
-      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
-    }
+  // State for storing chat messages
+  const [messages, setMessages] = useState([]);
 
-    // Customize speech bubble
-    const renderBubble = (props) => {
-      return <Bubble
+  // Append new messages to the existing messages on send
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  // Customize speech bubble
+  const renderBubble = (props) => {
+    return (
+      <Bubble
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: "#757083"
+            backgroundColor: "#757083",
           },
           left: {
-            backgroundColor: "#FFF"
-          }
+            backgroundColor: "#FFF",
+          },
         }}
       />
-    }
+    );
+  };
 
-    // Set the navigation title to the user's name upon component mount
-    useEffect(() => {
-        navigation.setOptions({ title: name });
-    }, []);
+  // Set the navigation title to the user's name upon component mount
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
 
-    useEffect(() => {
-        setMessages([
-          {
-            _id: 1,
-            text: "Hello developer",
-            createdAt: new Date(),
-            user: {
-              _id: 2,
-              name: "React Native",
-              avatar: "https://placeimg.com/140/140/any",
-            },
-          },
-          {
-            _id: 2,
-            text: 'This is a system message',
-            createdAt: new Date(),
-            system: true,
-          },
-        ]);
-    }, []);
+  // Load initial messages
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "This is a system message",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
 
-    return (
-        // View component as a container with dynamic background color based on user selection
-        <View style={[styles.container, { backgroundColor: background}]}>
-            <GiftedChat
-              messages={messages}
-              renderBubble={renderBubble}
-              onSend={messages => onSend(messages)}
-              user={{
-                _id: 1
-              }}
-            />
-            { 
-                Platform.OS === 'android' 
-                    ? <KeyboardAvoidingView behavior="height" /> 
-                    : null 
-            }
-        </View>
-    )
+  return (
+    <View style={[styles.container, { backgroundColor: background }]}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        // Use KeyboardAvoidingView to adjust the view when the keyboard is open
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, // Make the container use the full height of the screen
-        
-    }
+  container: {
+    flex: 1,
+  },
 });
 
 export default Chat;
