5172731e5eb195226033232480701b1951616b5f
MaGloCon <mgcontrada@gmail.com>
2024-07-02 14:30:24 +0200

    Integrated offline capabilities; Enhanced network awareness
    - In Chat.js, implement AsyncStorage for caching messages to support offline reading and enhance user experience.
    - Add conditional rendering of InputToolbar in Chat.js based on network connectivity, improving usability in offline scenarios.
    - Refactor message sending logic in Chat.js for better error handling and accessibility, including announcements for both success and failure.
    - Optimize Firestore message fetching in Chat.js with network status checks to reduce data usage and improve performance in offline mode.
    - Fix user object duplication in GiftedChat within Chat.js, ensuring accurate message attribution.
    - In App.js, integrate NetInfo to monitor network connectivity and pass this information to the Chat component, enabling dynamic handling of network changes.
    - Refactor App.js to include the initialization of NetInfo and pass the connectivity status to the Chat component as a prop.
    - Minor code cleanup and readability improvements in both Chat.js and App.js for maintainability.

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { LogBox } from 'react-native';
+import { useEffect } from 'react';
+import { LogBox, Alert } from 'react-native';
+import { useNetInfo } from "@react-native-community/netinfo";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
@@ -10,13 +12,15 @@ import Chat from './components/Chat';
 const Stack = createNativeStackNavigator();
 
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+// import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+// import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 //Main App component
 const App = () => {
+  // Check network connection status
+  const connectionStatus = useNetInfo();
   // Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyA0shx6YZVPnP_Iy2boC9CEWOieXzUFkkg",
@@ -29,9 +33,18 @@ const App = () => {
   
   const app = initializeApp(firebaseConfig); //Initialize Firebase
   const db = getFirestore(app); //Initialize Cloud Firestore and get a reference to the service
-  const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
+  // const auth = initializeAuth(app, {
+  //   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  // });
+
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
   
   return (
     <NavigationContainer>
@@ -45,7 +58,13 @@ const App = () => {
         <Stack.Screen
           name="Chat"
         >
-          {props => <Chat {...props} db={db} />}
+          {props => (
+            <Chat 
+              isConnected={connectionStatus.isConnected} 
+              db={db} 
+              {...props}
+            />
+          )}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,24 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform, AccessibilityInfo } from "react-native";
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { collection, addDoc, onSnapshot, orderBy, query } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db}) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const { name, background, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
   const onSend = (newMessages) => {
     // Append new messages to the previous messages
-    addDoc(collection(db, "messages"), newMessages[0])
+  addDoc(collection(db, "messages"), newMessages[0])
     .then(() => {
       // Announce the new message for screen reader users only if sending was successful
       AccessibilityInfo.announceForAccessibility("New message sent.");
     })
     .catch((error) => {
-      // Handle any errors here, such as by announcing the failure to send the message
       console.error("Error sending message: ", error);
     });
-  };
+};
 
   // Customize speech bubble
   const renderBubble = (props) => {
@@ -37,31 +37,57 @@ const Chat = ({ route, navigation, db}) => {
     );
   };
 
+  // Prevent rendering of InputToolbar when offline
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  };
+
   // Set the navigation title to the user's name upon component mount
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
   // Fetch messages from Firestore
+  // Messages database
+  let unsubMessages;
   useEffect(() => {
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-      const unsubMessages = onSnapshot(q, (documentSnapshot) => {
-          let newMessages = [];
-          documentSnapshot.forEach(doc => {
-            newMessages.push({ 
-              _id: doc.id, 
-              ...doc.data(),
-              createdAt: new Date(doc.data().createdAt.toMillis())
-            })
-          });
-          setMessages(newMessages);
-      });
-
-      // Unsubscribe from the collection when the component unmounts
-      return () => {
+    if (isConnected === true) {
+        // unregister current onSnapshot() listener to avoid registering multiple listeners when useEffect code is re-executed.
         if (unsubMessages) unsubMessages();
-      }
-  }, []);
+        unsubMessages = null;
+        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+        unsubMessages = onSnapshot(q, (documentSnapshot) => {
+            let newMessages = [];
+            documentSnapshot.forEach(doc => {
+              newMessages.push({ 
+                id: doc.id, 
+                ...doc.data(),
+                createdAt: new Date(doc.data().createdAt.toMillis())
+              })
+            });
+            cacheMessagesHistory(newMessages);
+            setMessages(newMessages);
+        });
+    } else loadCachedMessages();
+    // Clean up code
+    return () => {
+      if (unsubMessages) unsubMessages();
+    }
+  }, [isConnected]);
+
+  const loadCachedMessages = async () => {
+    const cachedMessages = await AsyncStorage.getItem("chat_messages") || [];
+    setMessages(JSON.parse(cachedMessages));
+  }
+
+  const cacheMessagesHistory = async (listsToCache) => {
+    try {
+      await AsyncStorage.setItem('chat_messages', JSON.stringify(listsToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
@@ -69,7 +95,7 @@ const Chat = ({ route, navigation, db}) => {
         messages={messages}
         renderBubble={renderBubble}
         onSend={(messages) => onSend(messages)}
-        user={{ _id: userID, name }}
+        user={{ userID, name }}
       />
       {Platform.OS === "android" ? (
         // Use KeyboardAvoidingView to adjust the view when the keyboard is open
